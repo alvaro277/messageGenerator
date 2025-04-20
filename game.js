770e5605bfe6a46e76626f24fc6afd94aaacb44f
alvaro277 <alvaropineda89can@gmail.com>
2025-04-20 20:31:24 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
 
 
-messageButton.addEventListener('click', () => {
+//messageButton.addEventListener('click', () => {
   //const arrayMessage = generateMessage(); 
   //message.innerText = formatMessage(arrayMessage);
-  const messageButton = document.getElementById('generator');
-  const message = document.getElementById('message');
-  message.innerText = "some text"
-}
+ // const messageButton = document.getElementById('generator');
+ // const message = document.getElementById('message');
+  //message.innerText = "some text"
+//}
     
-                              );
+//                              );
