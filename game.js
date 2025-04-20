106fe9b6d8630723774b20c8c562976c361bd16c
alvaro277 <alvaropineda89can@gmail.com>
2025-04-20 19:45:07 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 const messageButton = document.getElementById('generator');
 const message = document.getElementById('message');
 
-messageButton.addEventListener('click', function messageGen(){
+messageButton.addEventListener('click', () => {
   const arrayMessage = generateMessage(); 
   message.innerText(formatMessage(arrayMessage));
 }
