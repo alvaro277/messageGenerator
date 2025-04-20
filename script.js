3638e40be9453b1e48e910bb491e1c1aa8abbd5c
alvaro277 <alvaropineda89can@gmail.com>
2025-04-20 19:24:57 +0200

    new files to make app works

--- script.js
+++ script.js
@@ -3,11 +3,36 @@ function generateRandomNumber(num) {
     return Math.floor(Math.random() * num)
   }
   
-  const collectiveWisdom = {
-    signInfo: ['star', 'moon', 'sun', 'comet'],
-    fortuneOutput: ['terrible luck', 'bad luck', 'ok luck', 'good luck', 'great luck'],
-    advice: ['go out and eat', 'not read this', 'play more', 'trust no one']
-  }
+ 
   
   // Store the 'wisdom' in an array
-  let personalWisdom = []
+
+
+function generateMessage (){
+    const collectiveWisdom = {
+        'signInfo': ['star', 'moon', 'sun', 'comet'],
+        'fortuneOutput': ['terrible luck', 'bad luck', 'ok luck', 'good luck', 'great luck'],
+        'advice': ['go out and eat', 'not read this', 'play more', 'trust no one']
+      }
+    let personalWisdom = [];
+  for (let prop in collectiveWisdom){  
+    
+    let optionIndex = generateRandomNumber(collectiveWisdom[prop].length);
+    console.log(optionIndex);
+    switch (prop){
+        case 'signInfo' : personalWisdom.push(`Your sign right now is a ${collectiveWisdom['signInfo'][optionIndex]}`);
+        break;
+        case 'fortuneOutput' : personalWisdom.push(`You are having: ${collectiveWisdom['fortuneOutput'][optionIndex]}`);
+        break;
+        case 'advice' : personalWisdom.push(`You should ${collectiveWisdom['advice'][optionIndex]}`);
+        break;
+        default : personalWisdom.push('There is an error in the data provided');        
+    }
+  }
+
+  return personalWisdom;
+
+}
+const formatMessage = array => console.log(array.join('\n'));
+
+formatMessage(generateMessage());
